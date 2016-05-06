bdc4bac7ce57a1e8dd7e80acd054f8d6aedf07e6
Scott-Tatham <scottytatham@gmail.com>
2016-05-07 02:00:17 +1000

    Project Start
    Camera - first person
    Movement - wasd + turn mouse
    Level 1 - infinite staircase

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigid;
+    private float moveSpeed; 
+	private float jumpHeight;
+	private bool isGrounded; 
+
+	void Start()
+	{
+		Rigidbody rigid = GetComponent<Rigidbody>();
+        moveSpeed = 10.0f;
+        jumpHeight = 5.0f;
+        isGrounded = true;
+	}
+
+	void Update () 
+	{
+		playerInput (); 
+	}
+
+	void OnCollisionEnter(Collision collidedObject)
+	{
+		if (collidedObject.gameObject.name == "Ground")
+        {
+			isGrounded = true; 
+		}
+	}
+
+	private void playerInput()
+	{
+		{
+			if  (Input.GetKey(KeyCode.W) && isGrounded == true)
+            {
+				rigid.AddForce(rigid.transform.forward * moveSpeed, ForceMode.Acceleration);
+			}
+
+			if  (Input.GetKey(KeyCode.S) && isGrounded == true)
+            {
+				rigid.AddForce(rigid.transform.forward * -moveSpeed, ForceMode.Acceleration);
+			}
+
+			if (Input.GetKey (KeyCode.D) && isGrounded == true)
+            {
+				rigid.AddForce(rigid.transform.right * moveSpeed, ForceMode.Acceleration);
+			}
+
+            if (Input.GetKey(KeyCode.A) && isGrounded == true)
+            {
+                rigid.AddForce(rigid.transform.right * -moveSpeed, ForceMode.Acceleration);
+            }
+
+            if (Input.GetKeyDown (KeyCode.Space) && isGrounded == true)
+            {
+				rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+				isGrounded = false; 
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalScript : MonoBehaviour
+{
+    private Vector3 targetLocation;
+    public Vector3 jumpSize;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.transform.CompareTag("Player"))
+		{
+            Debug.Log("Poof!");
+            targetLocation = col.transform.position - jumpSize;
+			col.transform.position = targetLocation;
+		}
+	}
+}
