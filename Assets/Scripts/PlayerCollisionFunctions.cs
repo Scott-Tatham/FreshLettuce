2af44a48eb18788d6be15b8dd53556adf9043f6a
Scott-Tatham <scottytatham@gmail.com>
2016-05-08 02:40:25 +1000

    All Scripts

---
+++ Assets/Scripts/PlayerCollisionFunctions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionFunctions : MonoBehaviour
+{
+	public GameObject door;
+	public GameObject doorHinge;
+	private Animator animator;
+
+	void Start()
+	{
+		animator = doorHinge.GetComponent<Animator>();
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "DoorTrigger")
+        {
+			door.SetActive (true);
+			animator.SetBool("isOpened", true);
+        }
+
+        else
+        {
+			animator.SetBool("isOpened", false);
+		}
+	}
+}
--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class PortalScript : MonoBehaviour
 {
     private Vector3 targetLocation;
-    public Vector3 jumpSize;
+    public Transform door;
 
 	void OnTriggerEnter(Collider col)
 	{
-		if (col.transform.CompareTag("Player"))
+		if(col.transform.CompareTag("Player"))
 		{
-            Debug.Log("Poof!");
-            targetLocation = col.transform.position - jumpSize;
-			col.transform.position = targetLocation;
+            if (door.transform.position.z > 0)
+            {
+                targetLocation = new Vector3(door.transform.position.x, door.transform.position.y, door.transform.position.z - 1);
+                Debug.Log("Poof");
+                col.transform.position = targetLocation;
+            }
+
+            else
+            {
+                targetLocation = new Vector3(door.transform.position.x, door.transform.position.y, door.transform.position.z + 1);
+                Debug.Log("Poof");
+                col.transform.position = targetLocation;
+            }
 		}
 	}
 }
